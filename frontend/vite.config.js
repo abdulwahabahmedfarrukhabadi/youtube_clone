f28ad4e938badc21daba9ba20ff8493b82f59c8a
abdulwahabahmedkhanyusufzai <absurw@gmail.com>
2025-02-03 21:22:22 +0500

    comitted

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,23 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  
   plugins: [react()],
   optimizeDeps: {
     include: ['regenerator-runtime']
+  },
+  build: {
+    // Adjust chunk size warning limit
+    chunkSizeWarningLimit: 1000, // increases the warning threshold to 1MB
+    rollupOptions: {
+      output: {
+        // Manual chunking to split vendor libraries
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor' // Bundle all node_modules into a separate chunk
+          }
+        }
+      }
+    }
   }
- })
+})
+
